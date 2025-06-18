263481e230a4cea9e5eb951ff8fe9bcf2e8d99bb
edu-well <108446261+edu-well@users.noreply.github.com>
2025-06-18 07:47:18 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,392 @@
+const pdfDatabase = [
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2012 (Enonce)",
+        url: "https://drive.google.com/file/d/1KVDD17gomxMD20w_NtaD0B7MP-a880Oq/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2012 (Correction)",
+        url: "https://drive.google.com/file/d/1JAi-qrHN6aZnughOZt7GM9V-4A0JDtAO/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2014 (Enonce)",
+        url: "https://drive.google.com/file/d/1XJqZZV6XCGzu7mEa4r4wtarnJhR579yc/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2014 (Correction)",
+        url: "https://drive.google.com/file/d/1-Xzf-BZ2Ec_bvGp2SBU6AcfU_4XexjgP/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2015 (Enonce)",
+        url: "https://drive.google.com/file/d/13t2dXMP08jtqF-GJWu1-IfbE7sZQVsRM/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2015 (Correction)",
+        url: "https://drive.google.com/file/d/1n6KQ35ZAVip2uf3GDVOAt6uq3GaeFVgH/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2016 (Enonce)",
+        url: "https://drive.google.com/file/d/1-jXEXFwntDvKswrTgPfYfG30iJIcXLwC/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2016 (Correction)",
+        url: "https://drive.google.com/file/d/1xmoiMmJ424gFLy4Q7B6CJDHVjWdM2eEp/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2017 (Enonce)",
+        url: "https://drive.google.com/file/d/177NpDsNZHbjSDYejTMEtxGLYHOWqWJvj/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2017 (Correction)",
+        url: "https://drive.google.com/file/d/1jmY9iBETiDBapr7mhAvNjdG_JutftqF2/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2018 (Enonce)",
+        url: "https://drive.google.com/file/d/1bUCHUEqiYBRogDE5DBiHxVhJe5mKBlId/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2018 (Correction)",
+        url: "https://drive.google.com/file/d/1B-yokSZ_OP_kdemFz3nh1-wKcTHZvxNJ/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2019 (Enonce)",
+        url: "https://drive.google.com/file/d/1RUIEahG0yigKUsZXQ2U8ojJFneuQdnx0//preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2019 (Correction)",
+        url: "https://drive.google.com/file/d/11Rj2rvcX7_rt2XlfOE4QNGa1mpwzf69M/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2020 (Enonce)",
+        url: "https://drive.google.com/file/d/1KVDD17gomxMD20w_NtaD0B7MP-a880Oq/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2020 (Correction)",
+        url: "https://drive.google.com/file/d/1lP-JLkHzXxOiTQezoBjb-eYe01m1VIQ/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2021 (Enonce)",
+        url: "https://drive.google.com/file/d/1LL2ilCD2zezZcEec5eeOLFb1f7K1RBT6/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 1- 2021 (Correction)",
+        url: "https://drive.google.com/file/d/1x8XJWJqaNMhlhQKZqLjqXZCeZa0sXNis/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 1- 2012 (Enonce)",
+        url: "https://drive.google.com/file/d/15GLpeiQQU9SemH4oZ6rFtwTbo7LOHIJj/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 1- 2012 (Correction)",
+        url: "https://drive.google.com/file/d/1FyrOqIVZrqq2clPSMqcRBQscSmC3zt9L/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 1- 2014 (Enonce)",
+        url: "https://drive.google.com/file/d/1Q5Br4jGQb1T2drQwWQ37Bp0-GeifJoOA/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 1- 2014 (Correction)",
+        url: "https://drive.google.com/file/d/167zpIWdoNOcOALbFB7v5eVk7yL9NmpPl/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 1- 2015 (Enonce)",
+        url: "https://drive.google.com/file/d/17_OvMxT-yYAsXhPQK_n1N94aItOf38xc/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 1- 2015 (Correction)",
+        url: "https://drive.google.com/file/d/1pq48xKUgDAlDTmUV2p-EADZbkcmkHm7B/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 1- 2020 (Enonce)",
+        url: "https://drive.google.com/file/d/13gX-wa71RN5U4wDYQvf38omC-2zAO4Bt/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 1- 2020 (Correction)",
+        url: "https://drive.google.com/file/d/1YiyAYu2oy_ZJXvrGGIvJZ87k6YJpSjPt/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 1- 2021 (Enonce)",
+        url: "https://drive.google.com/file/d/17kCSdRD2dS8ZZfqDkzsGeqKDefDi92Pb/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 1- 2021 (Correction)",
+        url: "https://drive.google.com/file/d/1mlAphNpgpbweaBDVFmyavoPB-intlRsr/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 2- 2013 (Enonce)",
+        url: "https://drive.google.com/file/d/1SBQhJ8_xQIA64RgAon5Fdm1VeBfGrRKJ/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 2- 2013 (Correction)",
+        url: "https://drive.google.com/file/d/1RXIOBy4Z8jUMiC9RWWdY7AxnkEVJBfS8/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 2- 2014 (Enonce)",
+        url: "https://drive.google.com/file/d/1JM-1BEf-Yvy5VBg_bEgwyPtfwRrA2Vm4/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 2- 2014 (Correction)",
+        url: "https://drive.google.com/file/d/189G2nCKHMrWd8XtPgFVIkzW5wtz8MBjJ/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 2- 2015 (Enonce)",
+        url: "https://drive.google.com/file/d/1HRzHyOw6RNZ8AkaGZvgCtsIu8MVuWFue/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 2- 2015 (Correction)",
+        url: "https://drive.google.com/file/d/1qmXCw098GMkTZL3tTrGHq--GlDy3impg/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 2- 2016 (Enonce)",
+        url: "https://drive.google.com/file/d/1UY4niX9RO-0WrTYBCv6ofRItRZRXoy3E/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 2- 2016 (Correction)",
+        url: "https://drive.google.com/file/d/1D29LRS92Eo3P_yIZxibnHCeKwsSDurzc/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 2- 2020 (Enonce)",
+        url: "https://drive.google.com/file/d/1P--nuQq1yDDWSq889FUai_LazJwAiKWi/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 2- 2020 (Correction)",
+        url: "https://drive.google.com/file/d/1lXWxp_fItM5oIznmN_GvEonaq5HpL7BU/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 2- 2021 (Enonce)",
+        url: "https://drive.google.com/file/d/1JlFhm5EAS80b8hhkQgiAH0OIENnxZbN6/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 2- 2021 (Correction)",
+        url: "https://drive.google.com/file/d/1f3ESSFIUictkxAHDwiQ7u2ZbY5yS_6I6/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 2- 2022 (Enonce)",
+        url: "https://drive.google.com/file/d/1Cbe-LS6-XE35FkHLsbKKp-upoUkaBziX/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 2- 2022 (Correction)",
+        url: "https://drive.google.com/file/d/1XX9TNmaBqA2tDbd3uL8VSD5DQ2dr3dLE/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 2- 2014 (Enonce)",
+        url: "https://drive.google.com/file/d/1H3TfCE93Hx7WkMEvWd-yyXTQj1Cbk5Go/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 2- 2014 (Correction)",
+        url: "https://drive.google.com/file/d/1HX41PzCx5KD6-0Gb9CrsBWdbqAeOT8HY/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 2- 2019 (Enonce)",
+        url: "https://drive.google.com/file/d/1HBZ6tHZ3qq3v0N2GF-r4G8q5kgquvhXh/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 2- 2019 (Correction)",
+        url: "https://drive.google.com/file/d/1HeZPKGx8yPDOa3STnge-J6_has-1kifI/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 2- 2020 (Enonce)",
+        url: "https://drive.google.com/file/d/1HCYjBcLklzeaQD_ntRSR2799sVLm5uE8/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 2- 2020 (Correction)",
+        url: "https://drive.google.com/file/d/1Hh_YaXi_Ma-f2srSWYMdfy4ubgrdxT0U/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 2- 2022 (Enonce)",
+        url: "https://drive.google.com/file/d/1HKcmE8SxdPsq1ZdVQwBHEydW6o9IT5mu/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 2- 2022 (Correction)",
+        url: "https://drive.google.com/file/d/1HjA_kt0kzUg_BBObucqUrn0_9jVfHdzw/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 2- 2023 (Enonce)",
+        url: "https://drive.google.com/file/d/1HPYJtL4ixEzkNUaU-GAC97-IB7-VyYIs/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 2- 2024 (Correction)",
+        url: "https://drive.google.com/file/d/1HksYx5zV-UO5cRhL56rFl8p7qpSaxR9b/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2013 (Enonce)",
+        url: "https://drive.google.com/file/d/1HvErHezJ0vULfbs8rvH9jCbGM9W3x_cD/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2013 (Correction)",
+        url: "https://drive.google.com/file/d/1IZtMQVWpxEOpPLWi5PTGd3vhX9H-2fVa/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2014 (Enonce)",
+        url: "https://drive.google.com/file/d/1I2_cGo4iIma6TslLm56ccbnUuwiHLTnC/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2014 (Correction)",
+        url: "https://drive.google.com/file/d/1I_e99AKk4lqeaF435TBqGVc9fTa4Bgbe/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2016 (Enonce)",
+        url: "https://drive.google.com/file/d/1I80oy02KiZ4nV_bCXc2FWBVVDuWOcLh6/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2016 (Correction)",
+        url: "https://drive.google.com/file/d/1IaND1Hk_6RPY38azfnYCSW9XeZJXkIpb/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2017 (Enonce)",
+        url: "https://drive.google.com/file/d/1IDCoCO5gl17WwOEEfQqmoSQWvZcZsYXj/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2017 (Correction)",
+        url: "https://drive.google.com/file/d/1IbO4l2KvUa-ss6idMCUtEXYy_EY8MWVm/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2018 (Enonce)",
+        url: "https://drive.google.com/file/d/1IMRI3V9ZgyKGjLpw4AqBEsmrzM8Oc-r6/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2018 (Correction)",
+        url: "https://drive.google.com/file/d/1IlvlpobHvoe-d62I8ZgQo6zJB0rv9l7p/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2019 (Enonce)",
+        url: "https://drive.google.com/file/d/1IOOj3RyiL-taDCiDv458V7I_wBxovKkr/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2019 (Correction)",
+        url: "https://drive.google.com/file/d/1IokuVIrgHpHOLeWqsYGK_kkt1w0wS6II/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2022 (Enonce)",
+        url: "https://drive.google.com/file/d/1IRXSY3rf3aVmAUE382KpAR-Z427vHSuL/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2022 (Correction)",
+        url: "https://drive.google.com/file/d/1IpiY6QifRJqi5Z5QWNs2BAzrTPCfq0F6/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2023 (Enonce)",
+        url: "https://drive.google.com/file/d/1IXgHIVkpKiKSMCTb2oA5OXDGuN42z4qr/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de controle 3- 2023 (Correction)",
+        url: "https://drive.google.com/file/d/1ItzCQwyHgxzisg8XeoMkDbB4sLgqCUlS/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 3- 2018 (Enonce)",
+        url: "https://drive.google.com/file/d/1JMHz9_mR0ABUTgyHHvZ7AJlq0g796czz/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 3- 2018 (Correction)",
+        url: "https://drive.google.com/file/d/1Jp5NquXTYppaRGEmCLeeX3bny7q33P6k/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 3- 2019 (Enonce)",
+        url: "https://drive.google.com/file/d/1JSL155ADW9rayaPJ-dw-yXrlHBh1qPHm/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 3- 2019 (Correction)",
+        url: "https://drive.google.com/file/d/1KCPta8rF7hQzFoNNiI6P5ffo1a3RjSiK/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 3- 2021 (Enonce)",
+        url: "https://drive.google.com/file/d/1Jb6KxQqB_5-m69mb75fBTMKVL5qndUw2/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 3- 2021 (Correction)",
+        url: "https://drive.google.com/file/d/1KHbuVHF9GwkhaN-f730MA-FujjEcIspU/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 3- 2022 (Enonce)",
+        url: "https://drive.google.com/file/d/1JfD6Zwl_P_2A7FMf5xHcPF5bLmGDKb3D/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 3- 2022 (Correction)",
+        url: "https://drive.google.com/file/d/1KIHD2Z-US75Mmafh0LhtWV0Tt8lGnHov/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 3- 2023 (Enonce)",
+        url: "https://drive.google.com/file/d/1Jo-aFO0DxXu1at3RIQjrRzbLdoZTIHJ7/preview"
+    },
+    {
+        type: "pdf",
+        title: "Physique Devoir de synthese 3- 2023 (Correction)",
+        url: "https://drive.google.com/file/d/1KKElH4Hwu-cpGIZhFK5S9B_gZxA5e-I6/preview"
+    }
+];
